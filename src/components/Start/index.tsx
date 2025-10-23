78997268726b2be11f9532aae224f84244eb0dc5
서연 <s25037@gsm.hs.kr>
2025-10-23 23:38:43 +0900

    style: svg추가 및 퍼블리싱 수정

--- src/components/Start/index.tsx
+++ src/components/Start/index.tsx
@@ -1,10 +1,20 @@
-import Starst from '@/assets/Start.png';
+import { useNavigate } from 'react-router-dom';
+import StartImg from '../../assets/Start.png';
 
 const Start = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex justify-center bg-gray-300 min-h-screen">
-      <div className="w-[390px] h-screen bg-sky-400/70">
-        <img src={Starst} alt="Start" className="w-full h-auto"></img>
+    <div className="flex justify-center bg-white min-h-screen">
+      <div className="w-[390px] bg-sky-400/70 flex flex-col items-center">
+        <img src={StartImg} alt="Start" className="w-full h-auto" />
+
+        <button
+          onClick={() => navigate('/main')}
+          className="px-12 py-2 bg-blue-400 text-white rounded-xl hover:bg-blue-500 transition"
+        >
+          시작하기
+        </button>
       </div>
     </div>
   );
