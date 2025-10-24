36dfc5a73e09625b2a9daf5da45b297715d0502b
서연 <s25037@gsm.hs.kr>
2025-10-24 13:53:48 +0900

    feat: 충돌 수정

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 export { default as StartText } from './StartText';
-=======
-export { default as Home } from './Home.js';
-export { default as User } from './User.js';
->>>>>>> 2cb02056b2252d45c473ed9bfe250b4e42e53366
+export { default as Home } from './Home';
+export { default as User } from './User';
