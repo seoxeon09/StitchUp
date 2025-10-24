4e61651931a25f3db2616e2c86a56f9cb80f1d3e
서연 <s25037@gsm.hs.kr>
2025-10-24 13:46:07 +0900

    feat: 푸터 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,18 @@
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
+import { Footer } from './components';
 
+const Layout = () => {
+  const location = useLocation();
+
+  return <>{location.pathname !== '/' && <Footer />}</>;
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Layout />
+    </Router>
+  );
+};
+
+export default App;
